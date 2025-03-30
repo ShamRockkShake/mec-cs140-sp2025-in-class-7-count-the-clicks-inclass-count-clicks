3de24cf877543a257a778c718579b95cb3edc374
ShamRockkShake <Shamarmoncrieffe5@gmail.com>
2025-03-30 16:33:35 +0000

    button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// script.js
+let count = 0;
 
+function clickEvent() {
+  count++;
+  console.log(count);
+  document.getElementById("clickCount").textContent = "Click count: " + count;
+
+  if (count % 2 === 0) {
+    alert("Count is even: " + count);
+  }
+}
+
+document.getElementById("myButton").onclick = clickEvent;
